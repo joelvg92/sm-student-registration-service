b0ff2f03ededd6df6b18eaaef2b7246d8ed18c54
Joel Varghese <joel.varghese@ms3.com>
2023-03-07 23:17:18 -0500

    Cors update

--- src/main/java/com/samarthanam/registration/service/controllers/AuthController.java
+++ src/main/java/com/samarthanam/registration/service/controllers/AuthController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials = "false")
+@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials = "false",allowedHeaders = { "*" })
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
