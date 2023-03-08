e509a892e7afd547edf6f50f0e65dee0489078f1
Joel Varghese <joel.varghese@ms3.com>
2023-03-07 19:53:31 -0500

    Cors agian

--- src/main/java/com/samarthanam/registration/service/controllers/TestController.java
+++ src/main/java/com/samarthanam/registration/service/controllers/TestController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@CrossOrigin(origins = "*", maxAge = 3600)
-//@CrossOrigin(origins = "https://samarthanam-reg-service.herokuapp.com", maxAge = 3600, allowCredentials="true")
-@CrossOrigin
+@CrossOrigin(origins = "https://samarthanam-reg-service.herokuapp.com", maxAge = 3600, allowCredentials="false")
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
