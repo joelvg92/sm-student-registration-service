1ff1e6f7e82cc0f4db2d4e674c82c28cfb7fc5ae
Joel Varghese <joel.varghese@ms3.com>
2023-03-07 23:11:07 -0500

    Updated cors

--- src/main/java/com/samarthanam/registration/service/SpringBootLoginExampleApplication.java
+++ src/main/java/com/samarthanam/registration/service/SpringBootLoginExampleApplication.java
@@ -12,14 +12,4 @@ public class SpringBootLoginExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLoginExampleApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");;
-			}
-		};
-	}
 }
